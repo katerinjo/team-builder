a3bf3e4d0c11b1547165aa3e331c4a63af87cebd
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-24 13:37:25 -0700

    first attempt at using the form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import DictCard from "./components/DictCard";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
   const [team, setTeam] = useState([
-    { name: "Taylor Hebert", role: "DPS" },
-    { name: "Bonesaw", role: "healer" }
+    { name: "Bonesaw", role: "healer" },
+    { name: "Taylor Hebert", role: "DPS" }
   ]);
+
+  function addMember(newMember) {
+    setTeam([...team, newMember].sort());
+  }
+
   return (
     <div className="App">
       <div className="grid">
         {team.map(member => <DictCard data={member} />)}
       </div>
-
+      <Form submitFun={addMember} />
     </div>
   );
 }
