11c04a11b7bb33f15d5ed773d6db290a7851c81a
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-24 14:31:42 -0700

    form formatting and fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,6 +20,18 @@ function Form({ submitFun }) {
                 Name:
             <input type="text" id="name" value={member.name} onChange={inputHandler} />
             </label>
+            <label>
+                Role:
+            <input type="text" id="role" value={member.role} onChange={inputHandler} />
+            </label>
+            <label>
+                Power:
+            <input type="text" id="power" value={member.power} onChange={inputHandler} />
+            </label>
+            <label>
+                Email:
+            <input type="text" id="email" value={member.email} onChange={inputHandler} />
+            </label>
             <input type="submit" value="Add To Team" />
         </form>
     );
